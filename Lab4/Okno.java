49d46203dc9993a3962799ad231c4d0b4a0ead5f
barakkobama <jczykier@wp.pl>
2021-12-07 14:42:38 +0100

    Uciekanjący przycisk i kanwa do rysowania

--- Lab4/Okno.java
+++ Lab4/Okno.java
@@ -1,14 +1,16 @@
 package lab4;
-import javax.swing.*;
-import java.util.Random;
+import java.awt.CardLayout;
+import java.awt.Dimension;
 
-import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 public class Okno extends JFrame {
-	public static void main(String[] args) {
-		runMain();
-	}
 	
-	public static void runMain() {
+	static CardLayout cl = new CardLayout();
+	static JPanel panelCont,kanwa,escPanel;
+	
+	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				Okno okno = new Okno();
@@ -16,47 +18,25 @@ public class Okno extends JFrame {
 		});
 	}
 		
-	public static void checkIfCought(MouseEvent e,Random rnd,JButton runButton) {
-		if(e.getY()<20) {
-			System.out.println("Z³apany");
-		}
-		else {
-			int x = rnd.nextInt(395);
-			int y = rnd.nextInt(445);
-			runButton.setLocation(x,y);
-		}
-		
-	}
 	Okno(){
-		setSize(500,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(null);
-		JPanel panel = new JPanel();
-		panel.setBounds(5,5,495,495);
-		panel.setLayout(null);
-		setContentPane(panel);
+		panelCont = new JPanel();
+		panelCont.setLayout(cl);
+		panelCont.setPreferredSize(new Dimension(500,500));
+		escPanel = new EscButton();
+		kanwa = new Kanwa();
+		
+		panelCont.add(escPanel,"2");
+		panelCont.add(kanwa,"1");
+		
+		cl.show(panelCont,"1");
+		
+		add(panelCont);
 		setLocationRelativeTo(null);
-		Random rnd = new Random();
-		JButton runButton = new JButton("Run");
-		int startX = 200;
-		int startY = 200;
-		runButton.setBounds(startX,startY,100,50);
+		pack();
 		setVisible(true);
-		runButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				super.mouseEntered(e);
-				checkIfCought(e,rnd,runButton);
-			}
-		});
-		panel.add(runButton);
-		JButton resetButton = new JButton("Reset");
-		resetButton.setBounds(340,380,100,50);
-		resetButton.addActionListener(e->runButton.setLocation(startX,startY));
-		panel.add(resetButton);
-		JButton paintButton = new JButton("Zmien na rysowanie");
-		paintButton.setBounds(150,380,190,50);
-		panel.add(paintButton);
+		
+
 	}
 
 }
